4dda399453ed7747fca637f052135110ad470566
Ahmet Ahmedov <ahmet@hph.io>
2024-12-12 12:06:06 +0200

    Fix the test so it passes

--- test/MerkleAirdropTest.t.sol
+++ test/MerkleAirdropTest.t.sol
@@ -18,6 +18,7 @@ contract MerkleAirdropTest is Test, ZkSyncChainChecker {
     bytes32 proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
     bytes32 proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
     bytes32[] public PROOF = [proofOne, proofTwo];
+    address public gasPayer;
     address user;
     uint256 userPrivateKey;
 
@@ -34,16 +35,21 @@ contract MerkleAirdropTest is Test, ZkSyncChainChecker {
         }
 
         (user, userPrivateKey) = makeAddrAndKey("user");
+        gasPayer = makeAddr("gasPayer");
     }
 
     function testUsersCanClaim() public {
         uint256 startingBalance = token.balanceOf(user);
+        bytes32 digest = airdrop.getMessageHash(user, AMOUNT_TO_CLAIM);
 
-        vm.prank(user);
-        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF);
+        // sign a massage with the user's private key
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivateKey, digest);
+
+        // gasPayer will execute the claim function in behalf of the user using the user's signature
+        vm.prank(gasPayer);
+        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF, v, r, s);
 
         uint256 endingBalance = token.balanceOf(user);
-
         console.log("Ending balance: ", endingBalance);
 
         assertEq(endingBalance - startingBalance, AMOUNT_TO_CLAIM);
